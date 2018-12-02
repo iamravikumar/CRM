61030786896c5c4560954b71a7c4e2e1afab1eb7
Görkem Öztürk <gorkemozturk@hotmail.com>
2018-12-02 15:51:39 +0300

    Bootstrap imported to the project via Bower.

--- CRM/Startup.cs
+++ CRM/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CRM
@@ -15,6 +16,7 @@ namespace CRM
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +27,9 @@ namespace CRM
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
